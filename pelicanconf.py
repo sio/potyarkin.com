4d9da6b765e91879010f8ca8cdfbb3c310ff891d
Vitaly Potyarkin <sio.wtf@gmail.com>
2017-10-01 18:20:23 +0300

    Customize theme and settings for my blog

--- pelicanconf.py
+++ pelicanconf.py
@@ -31,7 +31,30 @@ SOCIAL = [
     ('github', 'https://github.com/sio'),
     ]
 
-DEFAULT_PAGINATION = 6
+DEFAULT_PAGINATION = 5
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+# Custom configuration for potyarkin.ml
+LOCALE = "C"
+DEFAULT_DATE_FORMAT = "%Y - %m - %d"
+DEFAULT_DATE = "fs"  # get post's date from filesystem
+
+ARTICLE_PATHS = ['posts',]
+ARTICLE_URL = '{category}/{date:%Y}/{slug}/'
+ARTICLE_SAVE_AS = '{category}/{date:%Y}/{slug}/index.html'
+YEAR_ARCHIVE_SAVE_AS = 'archive/{date:%Y}/index.html'
+PAGE_URL = '{slug}/'
+PAGE_SAVE_AS = '{slug}/index.html'
+
+THEME = "themes/pelican-hyde"
+PLUGIN_PATHS = [
+    'plugins/pelican-plugins']
+PLUGINS = [
+    ]
+SHARE_BUTTONS = [
+    'reddit',
+    'facebook',
+    'twitter',
+    ]
