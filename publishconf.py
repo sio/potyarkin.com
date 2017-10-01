ff581432ca7817c9341a5e40141eb5778d90efe1
Vitaly Potyarkin <sio.wtf@gmail.com>
2017-10-01 19:17:26 +0300

    Generate RSS feeds

--- publishconf.py
+++ publishconf.py
@@ -13,6 +13,8 @@ from pelicanconf import *
 SITEURL = 'http://potyarkin.ml'
 RELATIVE_URLS = False
 
+FEED_ALL_RSS = 'feeds/all.rss.xml'
+CATEGORY_FEED_RSS = 'feeds/%s.rss.xml'
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 
