d63bc12382c2bd264e34f834c41d062fd029fa29
Vitaly Potyarkin <sio.wtf@gmail.com>
2019-08-26 16:18:08 +0300

    Manage RSS/Atom feeds

--- publishconf.py
+++ publishconf.py
@@ -14,9 +14,8 @@ SITEURL = 'https://potyarkin.ml'
 RELATIVE_URLS = False
 
 FEED_ALL_RSS = 'feeds/all.rss.xml'
-CATEGORY_FEED_RSS = 'feeds/%s.rss.xml'
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+TAG_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
